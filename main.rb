bd1167d28fa5e7ed53b36e34664ac4fafdd5c057
Lee-Git22 <lee.alan9801@gmail.com>
2023-01-23 21:55:48 -0500

    Add comments for methods and clean code

--- main.rb
+++ main.rb
@@ -1,12 +1,13 @@
 # Methods for manipulating decoding board
 module DecodingBoard
+  # Outputs game rules
   def rules
     puts <<~TEXT
       +++ Rules of Mastermind +++
       ---------------------------
       + Available colors are:
       Red, Green, Blue, Yellow, Orange, Violet, Indigo
-      + Duplicates in colors are allowed
+      + Duplicates in code is NOT allowed
       + Codes are the initials of each color like RGBY
       + Correct position AND color is represented with !
       + Correct color only is represented with ?
@@ -15,29 +16,30 @@ module DecodingBoard
     TEXT
   end
 
+  # Shows current board configuration
   def showboard(mastermind)
-    puts '-----Current Board------'
-    mastermind[:decoding_board].each { |row| puts "++ #{row.to_s.gsub!('"', '')} ++" }
+    puts '-------MASTERMIND-------'
+    mastermind[:decoding_board].each { |row| puts "#{mastermind[:turn]}. #{row.to_s.gsub!('"', '')} ||" }
     puts '------------------------'
   end
 
+  # Creates new code peg from guess in X-X-X-X format
   def self.new_code_peg(guess)
     code_peg = ''
     guess.each_char { |color| code_peg = code_peg << color << '-' }
     code_peg.chop!
   end
 
+  # Creates new key peg from feedback in Y-Y-Y-Y format
   def self.new_key_peg(mastermind)
-    # Takes feedback and formats it like code_peg
     key_peg = ''
     mastermind[:feedback].each_char { |peg| key_peg = key_peg << peg if peg == '!' || peg == '?' }
-    until key_peg.length == 4
-      key_peg = key_peg + 'O'
-    end
+    key_peg += 'O' until key_peg.length == 4
     key_peg.gsub!('', '-')
     key_peg[1..].chop!
   end
 
+  # Adds formatted pegs to decoding board
   def self.add_peg(mastermind, code_peg, key_peg)
     mastermind[:decoding_board].push([code_peg, key_peg])
   end
@@ -47,11 +49,13 @@ end
 class Codebreaker
   include DecodingBoard
 
+  # Inputs user guess 
   def guess
     puts 'Enter your guess as 4 characters: (enter help for rules)'
     gets.chomp.upcase
   end
 
+  # Ensures guess is in correct format
   def guess_check(guess)
     if guess == 'HELP'
       rules
@@ -64,41 +68,29 @@ class Codebreaker
       end
     end
   end
-
 end
 
 # Methods for codemaker
 class Codemaker
   include DecodingBoard
 
+  # Creates a randomly selected code from valid color pool
   def make_code
     code = ''
     rng = Random.new
     until code.length == 4
-      tmp = VALID_COLORS[rng.rand(0...6)]
-      code = code << tmp unless code.include?(tmp)
+      color = VALID_COLORS[rng.rand(0...6)]
+      code += color unless code.include?(color)
     end
     code
   end
 
-  def check_color(mastermind)
-
-    4.times do |peg|
-      if mastermind[:feedback].include?(mastermind[:guess][peg])
-        mastermind[:feedback] = mastermind[:feedback] << '?'
-      end
-    end
-
-    mastermind[:feedback]
-  end
-
+  # Modify feedback by checking guess for correct position AND color
   def check_both(mastermind)
     mastermind[:feedback] = ''
-    
     4.times do |peg|
       if mastermind[:code][peg] == mastermind[:guess][peg]
         mastermind[:feedback] = mastermind[:feedback] << '!'
-
       else
         mastermind[:feedback] = mastermind[:feedback] << mastermind[:code][peg]
       end
@@ -106,24 +98,31 @@ class Codemaker
     mastermind[:feedback]
   end
 
-  def feedback
-    # Checks for correct positions and colors, then remove those indexes
-    # Checks for correct colors for remaining indexes
-    # Generate feedback based on count for 1 and 2
+  # Modify feedback by checking guess with modified code that excludes correct position and color pegs
+  def check_color(mastermind)
+    4.times do |peg|
+      mastermind[:feedback] += '?' if mastermind[:feedback].include?(mastermind[:guess][peg])
+    end
+    mastermind[:feedback]
+  end
+
+  # Creates complete feedback
+  def new_feedback(mastermind)
+    check_both(mastermind)
+    check_color(mastermind)
   end
 end
 
 mastermind = {
   player: '',
   cpu: '',
-  turns_left: 12,
+  turn: 1,
   decoding_board: [
   ],
   guess: '',
   code: '',
   feedback: ''
 }
-
 
 VALID_COLORS = ['R', 'G', 'B', 'Y', 'O', 'V', 'I']
 
@@ -146,18 +145,16 @@ VALID_COLORS = ['R', 'G', 'B', 'Y', 'O', 'V', 'I']
 mastermind[:player] = Codebreaker.new
 mastermind[:cpu] = Codemaker.new
 
-puts "code is: #{mastermind[:code] = mastermind[:cpu].make_code}"
+mastermind[:code] = mastermind[:cpu].make_code
+# puts "code is: #{mastermind[:code] = mastermind[:cpu].make_code}"
 
 until mastermind[:player].guess_check(mastermind[:guess]) != 'fail'
   mastermind[:guess] = mastermind[:player].guess
   code_peg = DecodingBoard.new_code_peg(mastermind[:guess])
 end
 
-
-
-puts mastermind[:feedback] = mastermind[:cpu].check_both(mastermind)
-puts mastermind[:feedback] = mastermind[:cpu].check_color(mastermind)
-
+mastermind[:feedback] = mastermind[:cpu].new_feedback(mastermind)
 key_peg = DecodingBoard.new_key_peg(mastermind)
+
 DecodingBoard.add_peg(mastermind, code_peg, key_peg)
 mastermind[:player].showboard(mastermind)
